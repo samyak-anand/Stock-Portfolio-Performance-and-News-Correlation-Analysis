2e54d26652e80d540cef6a8cecdf024e0b15faed
samyak-anand <samyak.anand@gmail.com>
2025-04-28 09:53:30 +0200

    update

--- Dags/logger.py
+++ Dags/logger.py
@@ -7,7 +7,7 @@ os.makedirs('logs', exist_ok=True)
 
 # Configure logging
 logging.basicConfig(
-    level=logging.NOTSET,
+    level=logging.INFO,
     format='%(asctime)s - %(levelname)s - %(name)s - %(message)s',
     handlers=[
         logging.FileHandler("logs/app.log"),
